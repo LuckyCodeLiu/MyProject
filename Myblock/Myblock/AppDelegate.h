50c4f90f73c2f54e294a7b2c4afd5fb0a3c64901
LuckyCodeLiu <HelloKitty_ccl@163.com>
2016-12-08 18:46:24 +0800

    测试看看

---
+++ Myblock/Myblock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myblock
+//
+//  Created by SLZX on 16/12/8.
+//  Copyright © 2016年 SLZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myblock/Myblock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Myblock
+//
+//  Created by SLZX on 16/12/8.
+//  Copyright © 2016年 SLZX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
